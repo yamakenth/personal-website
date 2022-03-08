e8e1ba54df800b4db2231078fc2b8a83d4fc8a73
Ken Yamada <yamakenth@gmail.com>
2022-03-08 17:15:07 +0700

    Ass react scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { FaBars } from 'react-icons/fa';
+import { Link } from 'react-scroll';
 
 function Header() {
   return (
@@ -8,10 +9,10 @@ function Header() {
         <h1 className='uppercase text-2xl font-semibold tracking-widest'>Ken Yamada</h1>
         {/* nav */}
         <ul className='hidden sm:flex justify-end items-center gap-12 uppercase font-light text-slate-600'>
-          <li><a href='#about'>About</a></li>
-          <li><a href='#skills'>Skills</a></li>
-          <li><a href='#projects'>Projects</a></li>
-          <li><a href='#contact'>Contact</a></li>
+          <li><Link to='about' spy={true} smooth={true}>About</Link></li>
+          <li><Link to='skills' spy={true} smooth={true}>Skills</Link></li>
+          <li><Link to='projects' spy={true} smooth={true}>Projects</Link></li>
+          <li><Link to='contact' spy={true} smooth={true}>Contact</Link></li>
         </ul>
         {/* mobile nav */}
         <FaBars className='block sm:hidden' size={24} />
