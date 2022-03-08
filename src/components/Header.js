46d6b34180cc77c6e42e154670ccab8aba8786b2
Ken Yamada <yamakenth@gmail.com>
2022-03-08 17:24:20 +0700

    Add ability to scroll to top of page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,37 @@ import { Link } from 'react-scroll';
 
 function Header() {
   return (
-    <header>
+    <header id='header'>
       <nav className='container mx-auto px-4 py-2 flex justify-between items-center'>
         {/* logo */}
-        <h1 className='uppercase text-2xl font-semibold tracking-widest'>Ken Yamada</h1>
+        <h1 className='uppercase text-2xl font-semibold tracking-widest'>
+          <Link to='header' smooth={true} className='cursor-pointer'>Ken Yamada</Link>
+        </h1>
+
         {/* nav */}
         <ul className='hidden sm:flex justify-end items-center gap-12 uppercase font-light text-slate-600'>
-          <li><Link to='about' spy={true} smooth={true}>About</Link></li>
-          <li><Link to='skills' spy={true} smooth={true}>Skills</Link></li>
-          <li><Link to='projects' spy={true} smooth={true}>Projects</Link></li>
-          <li><Link to='contact' spy={true} smooth={true}>Contact</Link></li>
+          <li>
+            <Link to='about' spy={true} smooth={true} className='cursor-pointer hover:text-sky-900'>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to='skills' spy={true} smooth={true} className='cursor-pointer hover:text-sky-900'>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to='projects' spy={true} smooth={true} className='cursor-pointer hover:text-sky-900'>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to='contact' spy={true} smooth={true} className='cursor-pointer hover:text-sky-900'>
+              Contact
+            </Link>
+          </li>
         </ul>
+        
         {/* mobile nav */}
         <FaBars className='block sm:hidden' size={24} />
       </nav>
