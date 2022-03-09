00de5b9bc860f4f16bcd31ed1d03358f10d21036
Ken Yamada <yamakenth@gmail.com>
2022-03-09 19:49:25 +0700

    Add icons for skills section

---
+++ src/assets/img/index.js
@@ -0,0 +1,53 @@
+import bootstrap from './bootstrap.svg';
+import css from './css.svg';
+import express from './express.svg';
+import firebase from './firebase.svg';
+import git from './git.svg';
+import github from './github.svg';
+import heroku from './heroku.svg';
+import html from './html.svg';
+import java from './java.svg';
+import javascript from './javascript.svg';
+import jest from './jest.svg';
+import mongodb from './mongodb.svg';
+import mongoose from './mongoose.png';
+import mysql from './mysql.svg';
+import nodejs from './nodejs.svg';
+import npm from './npm.svg';
+import passportjs from './passportjs.svg';
+import powerbi from './powerbi.svg';
+import pug from './pug.svg';
+import python from './python.svg';
+import r from './r.svg';
+import react from './react.svg';
+import tableau from './tableau.svg';
+import tailwindcss from './tailwindcss.svg';
+import webpack from './webpack.svg';
+
+export {
+  bootstrap,
+  css,
+  express,
+  firebase,
+  git,
+  github,
+  heroku,
+  html,
+  java,
+  javascript,
+  jest,
+  mongodb,
+  mongoose,
+  mysql,
+  nodejs,
+  npm,
+  passportjs,
+  powerbi,
+  pug,
+  python,
+  r,
+  react,
+  tableau,
+  tailwindcss,
+  webpack
+}
