21009cca4b456d53827ea064dbf4bad2664acf43
Ken Yamada <yamakenth@gmail.com>
2022-03-09 15:17:17 +0700

    Layout content

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,45 @@
 function Skills() {
   return (
     <section id='skills' className='bg-neutral-50'>
-      <div className='container py-6 h-96'>
+      <div className='container py-6'>
         <h2>Skills</h2>
+        <div className='mt-5'>
+          <h3>Languages</h3>
+          <p>JavaScript</p>
+          <p>SQL</p>
+          <p>Python</p>
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>Java</p>
+          <p>R</p>
+        </div>
+
+        <div className='mt-5'>
+          <h3>Frameworks/Libraries/Databases</h3>
+          <p>React.js</p>
+          <p>Node.js</p>
+          <p>Express.js</p>
+          <p>Passport.js</p>
+          <p>MongoDB</p>
+          <p>Mongoose</p>
+          <p>bootstrap</p>
+          <p>Tailwind</p>
+          <p>Pug</p>
+        </div>
+
+        <div className='mt-5'>
+          <h3>Miscellaneous</h3>
+          <p>Git</p>
+          <p>GitHub</p>
+          <p>NPM</p>
+          <p>Webpack</p>
+          <p>Heroku</p>
+          <p>Jest</p>
+          <p>Firebase</p>
+          <p>Tableau</p>
+          <p>Power BI</p>
+        </div>
+
       </div>
     </section>  
   );
