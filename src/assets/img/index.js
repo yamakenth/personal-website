bbbd3be294b3bf46332ddb27c067ed8173f4542e
Ken Yamada <yamakenth@gmail.com>
2022-03-09 20:10:47 +0700

    export icons as object of name and source

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -24,30 +24,33 @@ import tableau from './tableau.svg';
 import tailwindcss from './tailwindcss.svg';
 import webpack from './webpack.svg';
 
-export {
-  bootstrap,
-  css,
-  express,
-  firebase,
-  git,
-  github,
-  heroku,
-  html,
-  java,
-  javascript,
-  jest,
-  mongodb,
-  mongoose,
-  mysql,
-  nodejs,
-  npm,
-  passportjs,
-  powerbi,
-  pug,
-  python,
-  r,
-  react,
-  tableau,
-  tailwindcss,
-  webpack
-}
+// object of icons 
+const icons = {
+  bootstrap: { name: 'Bootstrap', iconSrc: bootstrap },
+  css: { name: 'CSS', iconSrc: css },
+  express: { name: 'Express', iconSrc: express },
+  firebase: { name: 'Firebase', iconSrc: firebase },
+  git: { name: 'Git', iconSrc: git },
+  github: { name: 'GitHub', iconSrc: github },
+  heroku: { name: 'Heroku', iconSrc: heroku },
+  html: { name: 'HTML', iconSrc: html },
+  java: { name: 'Java', iconSrc: java },
+  javascript: { name: 'JavaScript', iconSrc: javascript },
+  jest: { name: 'Jest', iconSrc: jest },
+  mongodb: { name: 'MongoDB', iconSrc: mongodb },
+  mongoose: { name: 'Mongoose', iconSrc: mongoose },
+  mysql: { name: 'MySQL', iconSrc: mysql },
+  nodejs: { name: 'Node.js', iconSrc: nodejs },
+  npm: { name: 'npm', iconSrc: npm },
+  passportjs: { name: 'Passport.js', iconSrc: passportjs },
+  powerbi: { name: 'Power BI', iconSrc: powerbi },
+  pug: { name: 'Pug', iconSrc: pug },
+  python: { name: 'Python', iconSrc: python },
+  r: { name: 'R', iconSrc: r },
+  react: { name: 'React.js', iconSrc: react },
+  tableau: { name: 'Tableau', iconSrc: tableau },
+  tailwindcss: { name: 'Tailwind', iconSrc: tailwindcss },
+  webpack: { name: 'Webpack', iconSrc: webpack }
+};
+
+export default icons;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,45 +1,46 @@
-import * as icon from '../assets/img';
+import icons from '../assets/img';
 
-function Skills() {
+function Skills() {  
+  
   return (
     <section id='skills' className='bg-neutral-50'>
       <div className='container py-6'>
         <h2>Skills</h2>
         <div className='mt-5'>
           <h3>Languages</h3>
-          <p><img className='skills-icon' src={icon.javascript} alt='JavaScript' />JavaScript</p>
-          <p><img className='skills-icon' src={icon.python} alt='Python' />Python</p>
-          <p><img className='skills-icon' src={icon.mysql} alt='MySQL' />MySQL</p>
-          <p><img className='skills-icon' src={icon.html} alt='HTML5' />HTML</p>
-          <p><img className='skills-icon' src={icon.css} alt='CSS3' />CSS</p>
-          <p><img className='skills-icon' src={icon.java} alt='Java' />Java</p>
-          <p><img className='skills-icon' src={icon.r} alt='R' />R</p>
+          <p><img className='skills-icon' src={icons.javascript.iconSrc} alt='JavaScript' />JavaScript</p>
+          <p><img className='skills-icon' src={icons.python.iconSrc} alt='Python' />Python</p>
+          <p><img className='skills-icon' src={icons.mysql.iconSrc} alt='MySQL' />MySQL</p>
+          <p><img className='skills-icon' src={icons.html.iconSrc} alt='HTML5' />HTML</p>
+          <p><img className='skills-icon' src={icons.css.iconSrc} alt='CSS3' />CSS</p>
+          <p><img className='skills-icon' src={icons.java.iconSrc} alt='Java' />Java</p>
+          <p><img className='skills-icon' src={icons.r.iconSrc} alt='R' />R</p>
         </div>
 
         <div className='mt-5'>
           <h3>Frameworks/Libraries/Databases</h3>
-          <p><img className='skills-icon' src={icon.react} alt='ReactJs' />React.js</p>
-          <p><img className='skills-icon' src={icon.nodejs} alt='NodeJs' />Node.js</p>
-          <p><img className='skills-icon' src={icon.express} alt='ExpressJs' />Express.js</p>
-          <p><img className='skills-icon' src={icon.passportjs} alt='PassportJs' />Passport.js</p>
-          <p><img className='skills-icon' src={icon.mongodb} alt='MongoDB' />MongoDB</p>
-          <p><img className='skills-icon' src={icon.mongoose} alt='Mongoose' />Mongoose</p>
-          <p><img className='skills-icon' src={icon.bootstrap} alt='Bootstrap' />bootstrap</p>
-          <p><img className='skills-icon' src={icon.tailwindcss} alt='TailwindCSS' />Tailwind</p>
-          <p><img className='skills-icon' src={icon.pug} alt='pug' />Pug</p>
+          <p><img className='skills-icon' src={icons.react.iconSrc} alt='ReactJs' />React.js</p>
+          <p><img className='skills-icon' src={icons.nodejs.iconSrc} alt='NodeJs' />Node.js</p>
+          <p><img className='skills-icon' src={icons.express.iconSrc} alt='ExpressJs' />Express.js</p>
+          <p><img className='skills-icon' src={icons.passportjs.iconSrc} alt='PassportJs' />Passport.js</p>
+          <p><img className='skills-icon' src={icons.mongodb.iconSrc} alt='MongoDB' />MongoDB</p>
+          <p><img className='skills-icon' src={icons.mongoose.iconSrc} alt='Mongoose' />Mongoose</p>
+          <p><img className='skills-icon' src={icons.bootstrap.iconSrc} alt='Bootstrap' />bootstrap</p>
+          <p><img className='skills-icon' src={icons.tailwindcss.iconSrc} alt='TailwindCSS' />Tailwind</p>
+          <p><img className='skills-icon' src={icons.pug.iconSrc} alt='pug' />Pug</p>
         </div>
 
         <div className='mt-5'>
           <h3>Miscellaneous</h3>
-          <p><img className='skills-icon' src={icon.git} alt='Git' />Git</p>
-          <p><img className='skills-icon' src={icon.github} alt='GitHub' />GitHub</p>
-          <p><img className='skills-icon' src={icon.npm} alt='npm' />npm</p>
-          <p><img className='skills-icon' src={icon.webpack} alt='Webpack' />Webpack</p>
-          <p><img className='skills-icon' src={icon.heroku} alt='Heroku' />Heroku</p>
-          <p><img className='skills-icon' src={icon.jest} alt='Jest' />Jest</p>
-          <p><img className='skills-icon' src={icon.firebase} alt='Firebase' />Firebase</p>
-          <p><img className='skills-icon' src={icon.tableau} alt='Tableau' />Tableau</p>
-          <p><img className='skills-icon' src={icon.powerbi} alt='Power BI' />Power BI</p>
+          <p><img className='skills-icon' src={icons.git.iconSrc} alt='Git' />Git</p>
+          <p><img className='skills-icon' src={icons.github.iconSrc} alt='GitHub' />GitHub</p>
+          <p><img className='skills-icon' src={icons.npm.iconSrc} alt='npm' />npm</p>
+          <p><img className='skills-icon' src={icons.webpack.iconSrc} alt='Webpack' />Webpack</p>
+          <p><img className='skills-icon' src={icons.heroku.iconSrc} alt='Heroku' />Heroku</p>
+          <p><img className='skills-icon' src={icons.jest.iconSrc} alt='Jest' />Jest</p>
+          <p><img className='skills-icon' src={icons.firebase.iconSrc} alt='Firebase' />Firebase</p>
+          <p><img className='skills-icon' src={icons.tableau.iconSrc} alt='Tableau' />Tableau</p>
+          <p><img className='skills-icon' src={icons.powerbi.iconSrc} alt='Power BI' />Power BI</p>
         </div>
 
       </div>
