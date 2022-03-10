36487bce3a0a9a691a83a074e77231ac3b54d0b4
Ken Yamada <yamakenth@gmail.com>
2022-03-10 23:42:29 +0700

    Add blank projects section and completed contact section to page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import About from './components/About';
-// import Contact from './components/Contact';
+import Contact from './components/Contact';
 import Header from './components/Header';
-// import Projects from './components/Projects';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 function App() {
@@ -21,10 +21,8 @@ function App() {
       <Header />
       <About />
       <Skills />
-      {/*
       <Projects />
       <Contact /> 
-      */}
     </div>
   );
 }
