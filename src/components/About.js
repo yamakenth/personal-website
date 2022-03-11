245163480628cf1ee2c61b421b71d246c1621222
Ken Yamada <yamakenth@gmail.com>
2022-03-11 22:01:25 +0700

    Change about text to outline

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,20 @@
 import { FaCode } from 'react-icons/fa';
 
 // CHANGE LATER
-const ABOUT_TEXT = `I'm currently an undergraduate looking to improve my software and web 
-development knowledge. I study at UC Berkeley, and I'm majoring in data science. My programming 
-journey kickstarted when learning to make a simple HTML website on Window's notepad program. The 
-Odin Project's Full Stack JavaScript Curriculum is just one of the ways I'm learning to code now. 
-Besides coding, I like to play board games and video games, watch MMA, and learn Spanish.`
+const ABOUT_TEXT = `I'm incoming uchicago cs student. graduated from ise at uw with honors. at college did ds research to quantify...
+Joined shopee (largest ecommerce platform in greater SE asia) as bi analyst and was promoted to team lead within a year.
+Decided to purse a more technical profession as I saw first hand how technology can amplify the impact and individual can have.
+I also enjoy the enviornment of continuos learning in tech industry. Decided to learn full-stack web dev as it 
+allows me to create a useful application. besides coding, I like to go the gym, read, follow tech news, and watch Formula 1.`
 
 function About() {
-  
   return (
     <section id='about' className='bg-neutral-50'>
       <div className='container py-16'>
         <h2 className='uppercase'>About</h2>
-        <div className='mt-5 flex items-center justify-center gap-12 px-12'>
-          <div className='w-1/2'>
-            <p>{ABOUT_TEXT}</p>
-          </div>
-          <FaCode size={180} />
+        <div className='mt-5 flex items-center justify-center gap-12 px-4'>
+          <p className='text-justify md:w-1/2 lg:w-7/12'>{ABOUT_TEXT}</p>
+          <FaCode size={180} className='hidden md:block' />
         </div>
       </div>
     </section>
