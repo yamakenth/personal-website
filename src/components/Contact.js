5b3c82e11efae2e4f8831079cd5d3860939e26cc
Ken Yamada <yamakenth@gmail.com>
2022-03-11 21:31:47 +0700

    Add padding

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -84,7 +84,7 @@ function Contact() {
 
   return (
     <section id='contact' className='bg-neutral-100'>
-      <div className='container py-6 flex flex-col items-center justify-between gap-10'>
+      <div className='container py-16 flex flex-col items-center justify-between gap-10'>
         <h2 className='self-start'>Contact</h2>
         <SocialNavLinks />
         <footer>
