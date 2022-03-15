6a387b5386e4cbb456caee83b8ad1bf3e89c1c11
Ken Yamada <yamakenth@gmail.com>
2022-03-15 15:24:27 +0700

    Import project data from external file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
-import blogApi from '../assets/img/projects/blog-api.gif';
 import { FaGithub, FaEye, FaCaretLeft, FaCaretRight, FaCircle } from 'react-icons/fa';
+
+import projects from '../project-data';
 
 function Projects() {
   return (
@@ -14,6 +15,7 @@ function Projects() {
           drop-shadow-md 
           p-6 
         '>
+          {/* Carousel control */}
           <div className='group hover:bg-neutral-900/40 absolute top-0 left-0 h-full rounded-l-xl flex flex-col items-center justify-center'>
             <button type='button'>
               <FaCaretLeft className='text-5xl text-neutral-500/60 group-hover:text-neutral-50/80' />
@@ -24,33 +26,34 @@ function Projects() {
               <FaCaretRight className='text-5xl text-neutral-500/60 group-hover:text-neutral-50/80' />
             </button>
           </div>
-
+          {/* Project details */}
           <div className='flex flex-col lg:flex-row justify-center items-center gap-6'>
-            <img src={blogApi} alt='' className='w-full lg:w-3/5 2xl:w-1/2 h-fit rounded-lg'/>
-            
+            <img src={projects[0].demoGif} alt='' className='w-full lg:w-3/5 2xl:w-1/2 h-fit rounded-lg'/>
             <div>
-              <h3 className='underline font-bold mb-2'>Members Only</h3>
-
-              <p className='mb-1'><span className='font-bold'>Summary:&nbsp;</span>This is a React client frontend for a blog web app built using the MERN stack.</p>
-
-              <p className='mb-1'><span className='font-bold'>Features:&nbsp;</span>A blog post contains a title, an author, a published date, blog content, and a list of comments. A comment contains a username (manual entry) of the author and comment content. Users can read blog posts (only published posts). Users can create and read comments.</p>
-
-              <p className='mb-1'><span className='font-bold'>Tech Stack:&nbsp;</span>JavaScript, React.js, Node.js, Express.js, Passport.js, MongoDB, Mongoose, HTML5, CSS3, Bootstrap</p>
-
+              <h3 className='underline font-bold mb-2'>{projects[0].name}</h3>
+              <p className='mb-1'>
+                <span className='font-bold'>Summary:&nbsp;</span>{projects[0].summary}
+              </p>
+              <p className='mb-1'>
+                <span className='font-bold'>Features:&nbsp;</span>{projects[0].features}
+              </p>
+              <p className='mb-1'>
+                <span className='font-bold'>Technologies Used:&nbsp;</span>{projects[0].technologies}
+              </p>
               <div className='flex justify-center items-center gap-2 mt-8'>
-                <a href='https://tailwindcss.com/docs/width' target='_blank' rel='noreferrer' className='btn btn-secondary'>
+                <a href={projects[0].demoLink} target='_blank' rel='noreferrer' className='btn btn-secondary'>
                   <FaEye />&nbsp;Demo
                 </a>
-                <a href='https://tailwindcss.com/docs/width' target='_blank' rel='noreferrer' className='btn btn-secondary'>
+                <a href={projects[0].repoLink} target='_blank' rel='noreferrer' className='btn btn-secondary'>
                   <FaGithub />&nbsp;Repo
                 </a>
               </div>
             </div>
           </div>
-
+          {/* Carousel Nav */}
           <div className='flex items-center justify-center gap-3 p-3 mt-6'>
             {
-              [...Array(6)].map((e, i) => {
+              [...Array(projects.length)].map((e, i) => {
                 return (
                   <button type='button' key={i}>
                     <FaCircle className='text-[0.5rem] text-neutral-500 hover:text-neutral-400 hover:scale-125' />
---
+++ src/project-data.js
@@ -0,0 +1,15 @@
+import blogApiDemo from './assets/img/projects/blog-api.gif';
+
+const projects = [
+  {
+    name: 'Members Only',
+    summary: 'This is a React client frontend for a blog web app built using the MERN stack.',
+    features: 'A blog post contains a title, an author, a published date, blog content, and a list of comments. A comment contains a username (manual entry) of the author and comment content. Users can read blog posts (only published posts). Users can create and read comments.',
+    technologies: 'JavaScript, React.js, Node.js, Express.js, Passport.js, MongoDB, Mongoose, HTML5, CSS3, Bootstrap',
+    demoGif: blogApiDemo,
+    demoLink: 'https://tailwindcss.com/docs/width',
+    repoLink: 'https://tailwindcss.com/docs/width'
+  }
+]
+
+export default projects;
