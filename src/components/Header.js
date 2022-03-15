6e2ca41ed8fef778ba179bf403b860e768d52454
Ken Yamada <yamakenth@gmail.com>
2022-03-15 23:02:57 +0700

    Refactor for readability

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,68 +2,40 @@ import { useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { animateScroll as scroll } from 'react-scroll';
 
-import HeaderNavLinks from './NavLinks';
+import NavLinks from './NavLinks';
 import SideNav from './SideNav';
 
-// create logo for header 
-function HeaderLogo() {
-  function scrollToTop() {
-    scroll.scrollToTop();
-  }
-
-  return (
-    <h1>
-      <button type='button' onClick={scrollToTop} className='uppercase font-semibold tracking-widest'>
-        Ken Yamada
-      </button>  
-    </h1>
-  );
-}
-
-// create header
-function Header() {
+const Header = () => {
   const [scrolled, setScrolled] = useState(false);
-  const [showMobileNav, setShowMobileNav] = useState(false);
+  const [showSideNav, setShowSideNav] = useState(false);
   
-  window.addEventListener('scroll', changeNavColor);
-
-  function changeNavColor() {
-    if (window.scrollY > 0) {
-      setScrolled(true);
-    } else {
-      setScrolled(false);
-    }
-  }
-
-  function handleShowMobileNavClick() {
-    setShowMobileNav(!showMobileNav);
-  }
+  const handleMenuButtonClick = () => setShowSideNav(true);
+  const handleLogoClick = () => scroll.scrollToTop();
+  
+  window.addEventListener('scroll', () => {
+    (window.scrollY > 0) ? setScrolled(true) : setScrolled(false)
+  });
   
   return (
-    <header 
-      id='header' 
-      className={`
-        sticky 
-        top-0 
-        z-50 
-        ${(!scrolled) 
-          ? 'bg-black/70 text-neutral-50 ' 
-          : 'bg-neutral-50 text-neutral-900 drop-shadow-md'}
-      `}
-    >
+    <header id='header' className={`sticky top-0 z-50 ${(!scrolled) ? 'bg-black/70 text-neutral-50' : 'bg-neutral-50 drop-shadow-md'}`}>
       <nav className='container py-2 flex justify-between items-center'>
-        <HeaderLogo />
+         {/* top-left logo */}
+        <button type='button' onClick={handleLogoClick}>
+          <h1 className='font-semibold tracking-widest'>Ken Yamada</h1>
+        </button>
         
-        <div className='lg:hidden'>
-          <button type='button' onClick={handleShowMobileNavClick} className='flex justify-center items-center'>
-            <FaBars size={24} />
-          </button>
-          <SideNav showNav={showMobileNav} handleShowNav={handleShowMobileNavClick} />
-        </div>
+        {/* mainnav */}
+        <NavLinks isMobile={false} className='hidden md:block' />
+        {/* menu/hamburger button */}
+        <button type='button' onClick={handleMenuButtonClick} className='md:hidden flex justify-center items-center'>
+          <FaBars className='text-2xl' />
+        </button>
         
-        <div className='hidden lg:block'>
-          <HeaderNavLinks isMobile={false} />
+        {/* sidenav */}
+        <div className={(showSideNav) ? 'block' : 'hidden'}>
+          <SideNav />
         </div>
+
       </nav>
     </header>
   );
