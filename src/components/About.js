3ed1afa6b055ac10f83966d4fbfc8567c27ca7e3
Ken Yamada <yamakenth@gmail.com>
2022-03-15 18:19:05 +0700

    Revise text in About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import { FaCode } from 'react-icons/fa';
 
 // CHANGE LATER
-const ABOUT_TEXT = `I'm incoming uchicago cs student. graduated from ise at uw with honors. at college did ds research to quantify...
-Joined shopee (largest ecommerce platform in greater SE asia) as bi analyst and was promoted to team lead within a year.
-Decided to purse a more technical profession as I saw first hand how technology can amplify the impact and individual can have.
-I also enjoy the enviornment of continuos learning in tech industry. Decided to learn full-stack web dev as it 
-allows me to create a useful application. besides coding, I like to go the gym, read, follow tech news, and watch Formula 1.`
+const ABOUT_TEXT = `I'm an incoming masters of computer science student at the University of 
+Chicago. I graduated from the University of Washington with a degree in industrial engineering and 
+worked as a business intelligence analyst at Shopee (largest e-commerce platform in greater 
+Southeast Asia). I decided to learn web development as I observed how technical skills can amplify 
+the amount of impact an individual can have on businesses. I also enjoy the environment of 
+continuous learning in tech industry. Besides coding, I enjoy going to the gym, reading, following 
+tech news, and watching Formula 1.`;
 
 function About() {
   return (
