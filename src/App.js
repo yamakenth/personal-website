26f420714a5fb8b031631b2ffe2eaffbc8c62a84
Ken Yamada <yamakenth@gmail.com>
2022-03-15 22:12:17 +0700

    Remove unnecessary tailwind classes

--- src/App.js
+++ src/App.js
@@ -5,21 +5,9 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
-function App() {
+const App = () => {
   return (
-    <div 
-      id='app' 
-      className='
-        text-neutral-900 
-        bg-neutral-50
-        bg-[url("./assets/img/about/bg-sakura.jpeg")]
-        sm:bg-[url("./assets/img/about/bg-sakura-md.jpeg")]
-        bg-no-repeat 
-        bg-fixed 
-        bg-contain
-        1.5xl:bg-cover
-      '
-    >
+    <div id='app' className='text-neutral-900 bg-[url("./assets/img/about/bg-sakura.jpeg")] sm:bg-[url("./assets/img/about/bg-sakura-md.jpeg")] bg-no-repeat bg-contain'>
       <Header />
       <Home />
       <About />
