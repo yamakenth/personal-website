6baf52d56314e21b8d54d0105aa397ccca1142c3
Ken Yamada <yamakenth@gmail.com>
2022-03-16 18:21:10 +0700

    Add animation to all contents of about section

--- src/components/About.js
+++ src/components/About.js
@@ -11,10 +11,10 @@ const About = () => {
   return (
     <section id='about' className='bg-neutral-50'>
       <div className='container py-16'>
-        <h2 className='uppercase'>About</h2>
+        <h2 ref={ref} className='uppercase'>About</h2>
         
-        <div ref={ref} className='mt-5 flex items-center justify-center gap-12 px-4'>
-          <p className={`${(isVisible) ? 'text-justify md:w-2/3 translate-x-0' : '-translate-x-[100vw]'} ease-linear duration-500`}>{aboutText}</p>
+        <div className={`mt-5 flex items-center justify-center gap-12 px-4 ${(isVisible) ? 'translate-x-0' : '-translate-x-[100vw]'} ease-linear duration-1000`}>  
+          <p className='text-justify md:w-2/3'>{aboutText}</p>
           <FaCode className='hidden md:block text-[10rem]' />
         </div>
 
