9589672ca04c65c8ea92e509c410f10e7a641c41
Ken Yamada <yamakenth@gmail.com>
2022-03-16 14:27:17 +0700

    Change background blur to make text more readable

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import { resume, homeText } from '../data/external-links';
 
 const Home = () => {
   return (
-    <section id='home' className='bg-gradient-to-b from-black/70 via-black/20 to-white/40'>
+    <section id='home' className='bg-gradient-to-b from-black/70 via-black/50 to-neutral-100/30'>
       <div className='container h-[21rem] xxs:h-96 xs:h-[26rem] sm:h-80 md:h-[50vw] xl:h-screen py-3 flex flex-col items-center justify-end sm:justify-center sm:-translate-x-[15%]'>
         <div className='flex flex-col justify-center items-center gap-2 xl:gap-6 border-2 xl:border-3 border-solid rounded-2xl border-neutral-50/50 p-2 xl:p-8'>
           
