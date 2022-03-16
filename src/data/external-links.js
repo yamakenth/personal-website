26f36104d22482a3c240e7c2399bf0b4e2b66005
Ken Yamada <yamakenth@gmail.com>
2022-03-16 14:24:55 +0700

    Resize Home section to accomodate for fixed header

--- src/data/external-links.js
+++ src/data/external-links.js
@@ -3,4 +3,6 @@ const linkedIn ='https://www.linkedin.com/in/yamakenth/';
 const gitHub='https://github.com/yamakenth';
 const resume ='https://drive.google.com/file/d/1bMAp7i-5VW5fCvf2T5T6E-TcE9nu7z17/view?usp=sharing';
 
-export { email, linkedIn, gitHub, resume };
+const homeText = 'Hello! My name is Ken and I\'m a self-taught full-stack web developer.';
+
+export { email, linkedIn, gitHub, resume, homeText };
