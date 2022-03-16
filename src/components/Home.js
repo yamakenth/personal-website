0564a1c56c94183d3bb7651b2a8313733c2c35e9
Ken Yamada <yamakenth@gmail.com>
2022-03-16 14:07:49 +0700

    Make background cover screen height for lg screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,62 +1,26 @@
 import { Link } from 'react-scroll';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 
-const INTRO_TEXT = 'Hello! My name is Ken and I\'m a self-taught full-stack web developer.';
-const RESUME ='https://drive.google.com/file/d/1bMAp7i-5VW5fCvf2T5T6E-TcE9nu7z17/view?usp=sharing';
+import { resume, homeText } from '../data/external-links';
 
-function Home() {
+const Home = () => {
   return (
     <section id='home' className='bg-gradient-to-b from-black/70 via-black/20 to-white/40'>
-      <div className='
-        container
-        h-72
-        xxs:h-80
-        xs:h-96
-        lg:h-[50vw]
-        py-3
-        flex
-        flex-col
-        items-center
-        sm:items-start
-        justify-end
-        sm:justify-center
+      <div className='container h-[21rem] xxs:h-96 xs:h-[26rem] lg:h-[50vw] xl:h-screen py-3 flex flex-col items-center justify-end
       '>
-        <div className='
-          flex 
-          flex-col 
-          justify-center 
-          items-center 
-          gap-3 
-          lg:gap-5
-          xl:gap-8
-          p-2 
-          xl:p-9
-          border-2 
-          border-solid 
-          rounded-2xl 
-          border-neutral-50/50
-          md:ml-20
-          lg:ml-38
-          xl:ml-56
-          md:mb-6
-          lg:-mb-6
-        '>
-          <h4 className='
-            text-neutral-50 
-            text-center
-            w-72
-            lg:w-96
-            lg:text-2xl
-            xl:text-3xl
-          '>
-            {INTRO_TEXT}
-          </h4>
-          <div className='flex justify-center items-center gap-2'>
-            <a href={RESUME} target='_blank' rel='noreferrer' className='btn btn-primary'>
-              Resume&nbsp;&nbsp;<FaExternalLinkAlt size={16} />
-            </a>
-            <Link to='projects' smooth={true} offset={-50} className='btn btn-secondary'>Projects</Link> 
+        {/* flex flex-col items-center sm:items-start justify-end sm:justify-center */}
+        
+        <div className='flex flex-col justify-center items-center border-2 border-solid rounded-2xl border-neutral-50/50'>
+          
+          {/* intro text */}
+          <h4 className='text-neutral-50 text-center'>{homeText}</h4>
+          
+          {/* button navs */}
+          <div className='flex justify-center items-center'>
+            <a href={resume} target='_blank' rel='noreferrer' className='btn btn-primary'>Resume&nbsp;<FaExternalLinkAlt /></a>
+            <Link to='projects' smooth={true} offset={-40} className='btn btn-secondary'>Projects</Link> 
           </div>
+
         </div>
       </div>
     </section>
