1ec26855cccd1cb9d698d4046b5c4e4c6a32dfe0
Ken Yamada <yamakenth@gmail.com>
2022-03-16 18:06:54 +0700

    Add transition to about text

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,20 @@
+import { useRef } from 'react';
 import { FaCode } from 'react-icons/fa';
 
 import { aboutText } from '../data/external-links';
+import useOnScreen from '../utilities';
 
 const About = () => {
+  const ref = useRef();
+  const isVisible = useOnScreen(ref);
+  
   return (
     <section id='about' className='bg-neutral-50'>
       <div className='container py-16'>
         <h2 className='uppercase'>About</h2>
         
-        <div className='mt-5 flex items-center justify-center gap-12 px-4'>
-          <p className='text-justify md:w-2/3'>{aboutText}</p>
+        <div ref={ref} className='mt-5 flex items-center justify-center gap-12 px-4'>
+          <p className={`${(isVisible) ? 'text-justify md:w-2/3 translate-x-0' : '-translate-x-[100vw]'} ease-linear duration-500`}>{aboutText}</p>
           <FaCode className='hidden md:block text-[10rem]' />
         </div>
 
---
+++ src/utilities.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+// React hook to determine when element is visible on view port
+const useOnScreen = (ref) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const observer = new IntersectionObserver(([entry]) => {
+    setIsIntersecting(entry.isIntersecting);
+  });
+
+  useEffect(() => {
+    observer.observe(ref.current);
+    return () => { observer.disconnect() };
+  });
+
+  return isIntersecting;
+}
+
+export default useOnScreen;
