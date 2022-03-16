f2dd4825457a98834f84162dc80c73eb5ab0dd00
Ken Yamada <yamakenth@gmail.com>
2022-03-16 15:16:58 +0700

    Revise texts for About section

--- src/data/external-links.js
+++ src/data/external-links.js
@@ -4,5 +4,11 @@ const gitHub='https://github.com/yamakenth';
 const resume ='https://drive.google.com/file/d/1bMAp7i-5VW5fCvf2T5T6E-TcE9nu7z17/view?usp=sharing';
 
 const homeText = 'Hello! My name is Ken and I\'m a self-taught full-stack web developer.';
+const aboutText = `I'm an incoming masters of computer science student at the University of 
+Chicago. I graduated from the University of Washington with a degree in industrial engineering and 
+worked as a business intelligence analyst at Shopee (largest e-commerce platform in greater 
+Southeast Asia). I decided to explore web development as I became interested in learning how 
+technologies we use everyday function under the hood. Besides coding, I enjoy going to the gym, 
+reading, following tech news, and watching Formula 1.`;
 
-export { email, linkedIn, gitHub, resume, homeText };
+export { email, linkedIn, gitHub, resume, homeText, aboutText };
