f5c18272c604889b2a2b4c9659e1f03d12845d96
Ken Yamada <yamakenth@gmail.com>
2022-03-16 14:08:46 +0700

    Change bg image

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Skills from './components/Skills';
 
 const App = () => {
   return (
-    <div id='app' className='text-neutral-900 bg-[url("./assets/img/background/bg-sakura.jpeg")] sm:bg-[url("./assets/img/background/bg-sakura-md.jpeg")] bg-no-repeat bg-contain'>
+    <div id='app' className='text-neutral-900 bg-[url("./assets/img/background/bg-sakura.jpeg")] sm:bg-[url("./assets/img/background/bg-sakura-sm.jpeg")] lg:bg-[url("./assets/img/background/bg-sakura-lg.jpeg")] bg-no-repeat bg-contain'>
       <Header />
       <Home />
       <About />
