51b7c0fe915d96923c7422c51f4ef6dc92e1c4e7
Ken Yamada <yamakenth@gmail.com>
2022-04-05 11:39:14 +0700

    Change university name

--- src/data/external-links.js
+++ src/data/external-links.js
@@ -6,10 +6,10 @@ const sourceCode = 'https://github.com/yamakenth/personal-website';
 
 const homeText = 'Hello! My name is Ken and I\'m a self-taught full-stack web developer.';
 const aboutText = `I'm an incoming masters of computer science student at the University of 
-Chicago. I graduated from the University of Washington with a degree in industrial engineering and 
-worked as a business intelligence analyst at Shopee (largest e-commerce platform in greater 
-Southeast Asia). I decided to explore web development as I became interested in learning how 
-technologies we use everyday function under the hood. Besides coding, I enjoy going to the gym, 
-reading, following tech news, and watching Formula 1.`;
+Pennsylvania for Fall 2022. I graduated from the University of Washington with a degree in 
+industrial engineering and worked as a business intelligence analyst at Shopee (largest e-commerce 
+platform in greater Southeast Asia). I decided to explore web development as I became interested in 
+learning how technologies we use everyday function under the hood. Besides coding, I enjoy going to 
+the gym, reading, following tech news, and watching Formula 1.`;
 
 export { email, linkedIn, gitHub, resume, homeText, aboutText, sourceCode };
