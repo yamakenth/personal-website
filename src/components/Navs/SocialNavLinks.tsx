b5fb857e8fadc896376d2c6925afd29920f6c773
Ken Yamada <yamakenth@gmail.com>
2022-07-28 15:35:46 +0700

    Use css to control hover text display

--- src/components/Navs/SocialNavLinks.tsx
+++ src/components/Navs/SocialNavLinks.tsx
@@ -8,90 +8,74 @@ import {
 } from "react-icons/fa";
 
 import { email, gitHub, linkedIn, resume } from "../../data/external-links";
-import { useHover } from "../../utilities";
+
+const NAV_LINKS = [
+  {
+    href: `mailto:${email}`,
+    hoverColor: "hover:text-yellow-500",
+    icon: <FaEnvelope className="text-2xl" />,
+    defaultText: "Email",
+    hoverText: email,
+  },
+  {
+    href: linkedIn,
+    hoverColor: "hover:text-[#0e76a8]",
+    icon: <FaLinkedin className="text-2xl" />,
+    defaultText: "LinkedIn",
+    hoverText: "yamakenth",
+  },
+  {
+    href: gitHub,
+    hoverColor: "hover:text-black",
+    icon: <FaGithubSquare className="text-2xl" />,
+    defaultText: "GitHub",
+    hoverText: "yamakenth",
+  },
+  {
+    href: resume,
+    hoverColor: "hover:text-[#1fa463]",
+    icon: <FaFileAlt className="text-2xl" />,
+    defaultText: "Resume",
+    hoverText: "Open in Google Drive",
+  },
+];
 
 export default function SocialNavLinks(): JSX.Element {
-  const [emailIsHovering, emailHoverProps] = useHover();
-  const [linkedinIsHovering, linkedinHoverProps] = useHover();
-  const [githubIsHovering, githubHoverProps] = useHover();
-  const [resumeIsHovering, resumeHoverProps] = useHover();
-
   return (
     <ul className="flex flex-col items-center justify-between gap-4 text-xl">
-      <li>
-        <a
-          href={`mailto:${email}`}
-          className="contact-navitem hover:text-yellow-500"
-          {...emailHoverProps}
-        >
-          <FaEnvelope className="text-2xl" />
-          {emailIsHovering ? (
-            <span className="contact-navitem-span">
-              {email}
+      {NAV_LINKS.map((link) => (
+        <li key={link.defaultText}>
+          <a
+            href={link.href}
+            target="_blank"
+            rel="noreferrer"
+            className={`
+              flex gap-2 
+              items-center 
+              transition-all 
+              duration-500 
+              group
+              ${link.hoverColor} 
+            `}
+          >
+            {link.icon}
+            <span className="flex gap-2 items-center group-hover:hidden">
+              {link.defaultText}
+            </span>
+            <span
+              className="
+                hidden 
+                group-hover:flex 
+                gap-2 
+                items-center 
+                group-hover:underline"
+            >
+              {link.hoverText}
               <FaExternalLinkAlt />
             </span>
-          ) : (
-            "Email"
-          )}
-        </a>
-      </li>
-      <li>
-        <a
-          href={linkedIn}
-          target="_blank"
-          rel="noreferrer"
-          className="contact-navitem hover:text-[#0e76a8]"
-          {...linkedinHoverProps}
-        >
-          <FaLinkedin className="text-2xl" />
-          {linkedinIsHovering ? (
-            <span className="contact-navitem-span">
-              yamakenth
-              <FaExternalLinkAlt />
-            </span>
-          ) : (
-            "LinkedIn"
-          )}
-        </a>
-      </li>
-      <li>
-        <a
-          href={gitHub}
-          target="_blank"
-          rel="noreferrer"
-          className="contact-navitem"
-          {...githubHoverProps}
-        >
-          <FaGithubSquare className="text-2xl" />
-          {githubIsHovering ? (
-            <span className="contact-navitem-span">
-              yamakenth
-              <FaExternalLinkAlt />
-            </span>
-          ) : (
-            "GitHub"
-          )}
-        </a>
-      </li>
-      <li>
-        <a
-          href={resume}
-          target="_blank"
-          rel="noreferrer"
-          className="contact-navitem hover:text-[#1fa463]"
-          {...resumeHoverProps}
-        >
-          <FaFileAlt className="text-2xl" />
-          {resumeIsHovering ? (
-            <span className="contact-navitem-span">
-              Open in Google Drive
-              <FaExternalLinkAlt />
-            </span>
-          ) : (
-            "Resume"
-          )}
-        </a>
-      </li>
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
